a1fb11b11f12d703ef85ec4605c7fac70c21920b
James <jamesdix@cwgsy.net>
2016-12-19 14:43:38 +0000

    skips to next frame if strike

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -10,6 +10,8 @@ describe("Bowling", function(){
 
     it("should score the sum of roll 1 and 2 - no specials", function(){
       expect(bowling.frameScore(bowling.rollOne(3), bowling.rollTwo(6))).toEqual(9)
+      expect(bowling.scorecard).toEqual([9])
+      expect(bowling.frame).toEqual(2)
     });
 
     it("should keep keep count of the frame number", function(){
@@ -17,6 +19,13 @@ describe("Bowling", function(){
       bowling.frameScore(bowling.rollOne(1), bowling.rollTwo(6));
       bowling.frameScore(bowling.rollOne(4), bowling.rollTwo(4));
       expect(bowling.frame).toEqual(4);
+      expect(bowling.scorecard).toEqual([9,7,8])
+    });
+
+    it("should move to next frame if rollOne is a strike", function(){
+      bowling.frameScore(bowling.rollOne(10));
+      expect(bowling.scorecard).toEqual([10]);
+      expect(bowling.frame).toEqual(2)
     });
   });
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,7 +1,7 @@
 var Bowling = function(){
 
+  this.scorecard = [];
   this.frame = 1
-
 };
 
 Bowling.prototype.rollOne = function(number){
@@ -12,7 +12,8 @@ Bowling.prototype.rollTwo = function(number){
   return number
 };
 
-Bowling.prototype.frameScore = function(rollOne, rollTwo){
+Bowling.prototype.frameScore = function(rollOne, rollTwo = 0){
+  this.scorecard.push(rollOne + rollTwo)
   this.frame += 1
   return rollOne + rollTwo
 };
